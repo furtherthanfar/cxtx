12fece9368d6a00ff522314457fb8b325239199f
qiuqiu <hqsqiuqiu@163.com>
2020-09-01 22:32:13 +0800

    Initial commit

---
+++ src/main/java/com/cxtx/demo/CxtxApplication.java
@@ -0,0 +1,13 @@
+package com.cxtx.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CxtxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CxtxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cxtx/demo/CxtxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cxtx.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CxtxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
