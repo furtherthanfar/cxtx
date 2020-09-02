37940c248ec4aa1fa74ce31a1b5eac33ca502c36
刘乾坤 <Universe_Liu@163.com>
2020-09-02 15:34:43 +0800

    增加一个容器工具类

--- src/main/java/com/cxtx/demo/CxtxApplication.java
+++ src/main/java/com/cxtx/demo/CxtxApplication.java
@@ -1,13 +1,18 @@
 package com.cxtx.demo;
 
+import com.cxtx.context.ContextUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class CxtxApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CxtxApplication.class, args);
+        // SpringBoot启动
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(CxtxApplication.class, args);
+        // 容器工具类设置
+        ContextUtil.setApplicationContext(applicationContext);
     }
 
 }
--- src/main/java/com/cxtx/demo/HelloWorldController.java
+++ src/main/java/com/cxtx/demo/HelloWorldController.java
@@ -1,25 +1,12 @@
 package com.cxtx.demo;
 
-import org.apache.tomcat.util.json.JSONParser;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class HelloWorldController {
 
 
-    @RequestMapping(value = "helloWorld")
-    public String helloWorld(){
-        ModelAndView mv = new ModelAndView();
-
-        mv.addObject("name","Tom");
-        mv.setViewName("fafaf.html");
-
-
-        return "HelloWorld";
-    }
 
 
 }
